815365a00e615fc5407c8953df66e11d55a8e164
Kelvinm <kelvinmurithi18@gmail.com>
2025-03-11 21:22:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 =20
+let num4 =8
+let multiply= (num1 *num2)
+let random = Math.ceil(Math.random() > 0)
+let mod = num3%num4
+let max = Math.max(1,9,12,20)
